ed7f8e03af80256806aad4463485b8d63cd9aa1f
Ksu <ksuwa2711@gmail.com>
2016-02-26 17:53:30 +0300

    resize photos portfolio

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
 
 gulp.task('default', function () {
-	gulp.src('test.png')
-	.pipe(imageResize({
-		width : 100,
-		height : 100,
-		crop : true,
-		upscale : false
-	}))
-	.pipe(gulp.dest('dist'));
+	gulp.src('img/portfolio/main/*/*.jpg')
+		.pipe(imageResize({
+			width : 300,
+			height : 200,
+			crop : true,
+			upscale : false
+		}))
+		.pipe(gulp.dest('img/portfolio/mini'));
 });
