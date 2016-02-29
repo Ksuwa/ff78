39a2779185990426c4132a59e732cd2fece39445
Ksu <ksuwa2711@gmail.com>
2016-02-29 18:34:02 +0300

    all portfolio done

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
+var watermark = require("gulp-watermark");
 
 gulp.task('default', function () {
-	gulp.src('img/portfolio/main/*/*.jpg')
+	gulp.src('img/portfolio/scr-main/*/*.{jpg,JPG}')//resize big images to small
 		.pipe(imageResize({
 			width : 300,
 			height : 200,
@@ -10,4 +11,12 @@ gulp.task('default', function () {
 			upscale : false
 		}))
 		.pipe(gulp.dest('img/portfolio/mini'));
+
+gulp.src('img/portfolio/scr-main/*/*.{jpg,JPG}')//add watermark to photos
+	.pipe(watermark({
+		image: 'img/watermark.png',
+		resize: '50%',
+		gravity: 'South'
+	}))
+	.pipe(gulp.dest('img/portfolio/main'));
 });
---
+++ js/fancybox.js
@@ -0,0 +1,26 @@
+	$(function () {
+	$('.fancybox').fancybox({
+		type		: 'image', // to open placeholder images in fancybox;
+		openEffect	: 'elastic',
+		openSpeed	: 100,
+
+		overlay : {
+			css : {
+				'background' : 'rgba(58, 42, 45, 0.95)'
+			}
+		},
+
+		closeClick	: true,
+		closeEffect : 'elastic',
+		closeSpeed	: 100,
+
+		helpers: {
+			title: {
+				type : 'inside'
+			},
+			overlay: {
+				locked : false
+			}
+		}
+	});
+	});
