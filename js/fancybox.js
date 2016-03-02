ec9c5b266cc3c248fb167da20f509c8a6877a25c
Ksu <ksuwa2711@gmail.com>
2016-03-02 14:14:58 +0300

    After code review2

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,4 +1,4 @@
-	$(function () {
+$(function () {
 	$('.fancybox').fancybox({
 		type		: 'image', // to open placeholder images in fancybox;
 		openEffect	: 'elastic',
@@ -23,4 +23,4 @@
 			}
 		}
 	});
-	});
+});
